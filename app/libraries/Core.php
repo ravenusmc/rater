c24aec5a786d969b8c0845c1d8bac86d845b676c
ravenusmc <mcuddy77@gmail.com>
2020-10-28 08:04:23 -0400

    worked on the core file adding a method to route to controllers

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,14 +10,32 @@
 		protected $params = [];
 
 		public function __construct() {
-			$this->getUrl();
+			// print_r($this->getUrl());
+			$url = $this->getUrl();
+
+			// Look into controllers for first index in url array
+			if (file_exists('../app/controllers//' . ucwords($url[0]) . '.php')) {
+				// If exists, set as controller 
+				$this->currentController = ucwords($url[0]);
+				// unset 0 index 
+				unset($url[0]);
+			}
+
+			// Require the controller 
+			require_once '../app/controllers/' . $this->currentController . '.php';
+
+			// Instantiate Controller 
+			$this->currentController = new $this->currentController;
 		}
 
 		public function getUrl() {
-			echo $_GET['url'];
-			// if() {
-
-			// }
+			// echo $_GET['url'];
+			if(isset($_GET['url'])) {
+				$url = rtrim($_GET['url'], '/');
+				$url = filter_var($url, FILTER_SANITIZE_URL);
+				$url = explode('/', $url);
+				return $url;
+			}
 		}
 
 	}
