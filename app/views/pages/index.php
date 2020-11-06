76704bfb855d123b198c728f4480cf5caee56362
ravenusmc <mcuddy77@gmail.com>
2020-11-06 08:05:05 -0500

    working on home page

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,10 +1,16 @@
 <?php require APPROOT . "/views/inc/header.php"; ?>
+<link rel='stylesheet' href="<?php echo URLROOT; ?>/css/home.css">
 
 <div class="jumbotron jumbotron-fluid text-center">
-    <div class="container">
-      <h1 class='display-3'><?php echo $data['title'] ?></h1>
-      <p class='lead'><?php echo $data['description'] ?></p>
-    </div>
+  <div class="container">
+    <h1 class='display-3'><?php echo $data['title'] ?></h1>
+    <p class='lead'><?php echo $data['description'] ?></p>
   </div>
+</div>
+
+<main> 
+
+</main>
+
 
 <?php require APPROOT . "/views/inc/footer.php"; ?>
