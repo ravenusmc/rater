74cbd5b2f3204478c3b7e665c37b23623d3c144c
ravenusmc <mcuddy77@gmail.com>
2020-11-07 13:26:13 -0500

    working on landing page

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,8 +1,8 @@
 <?php require APPROOT . "/views/inc/header.php"; ?>
 <link rel='stylesheet' href="<?php echo URLROOT; ?>/css/home.css">
 
-<div class="jumbotron jumbotron-fluid text-center">
-  <div class="container">
+<div class="jumbotron jumbotron-fluid text-center jumbotron_container">
+  <div class='jumbotron_div_area'>
     <h1 class='display-3'><?php echo $data['title'] ?></h1>
     <p class='lead'><?php echo $data['description'] ?></p>
   </div>
