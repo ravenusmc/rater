ab2ebbb67d12b8ade687f70ef3b0802735e148a7
ravenusmc <mcuddy77@gmail.com>
2020-11-08 15:08:46 -0500

    worked on registration page

---
+++ app/controllers/Users.php
@@ -0,0 +1,33 @@
+<?php 
+
+	class Users extends Controller {
+
+		public function __construct() {
+
+		}
+
+		public function register() {
+			// Check for Posts 
+			if($_SERVER['REQUEST_METHOD'] == 'POST') {
+					// Process Form
+
+			}else {
+				// Init Data 
+				$data = [
+					'name' => '',
+					'email' => '',
+					'password' => '',
+					'confirm_password' => '',
+					'name_err' => '',
+					'email_err' => '',
+					'password_err' => '',
+					'confirm_password_err' => ''
+				];
+
+				// Load View
+				$this->view('users/register', $data);
+
+			}
+		}
+
+	}
