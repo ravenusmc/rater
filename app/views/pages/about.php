044b899e9b3948358445553f21086dc107788abb
ravenusmc <mcuddy77@gmail.com>
2020-11-20 06:24:35 -0500

    added in and used new font

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,8 +1,9 @@
 <?php require APPROOT . "/views/inc/header.php"; ?>
+<link rel='stylesheet' href="<?php echo URLROOT; ?>/css/generic.css">
 <link rel='stylesheet' href="<?php echo URLROOT; ?>/css/about.css">
 
 	<!-- Header area -->
-	<div class='about_header'>
+	<div class='about_header font'>
 		<h1 class='title font center'>About Me</h1>
 	</div>
 	<!-- End of Header Area -->
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,14 +2,14 @@
 <link rel='stylesheet' href="<?php echo URLROOT; ?>/css/generic.css">
 <link rel='stylesheet' href="<?php echo URLROOT; ?>/css/home.css">
 
-<div class="jumbotron jumbotron-fluid text-center jumbotron_container">
+<header role='banner' class="jumbotron jumbotron-fluid text-center jumbotron_container">
   <div class='jumbotron_div_area'>
-    <h1 class='display-3'><?php echo $data['title'] ?></h1>
-    <p class='lead'><?php echo $data['description'] ?></p>
+    <h1 class='display-3 font'><?php echo $data['title'] ?></h1>
+    <p class='lead font'><?php echo $data['description'] ?></p>
   </div>
-</div>
+</header>
 
-<main> 
+<main role="main"> 
 
   <section>
     <div class='description-div'>
