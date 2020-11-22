25eda6b8f3e01ab7cb37949fae89d9701d23bcf4
ravenusmc <mcuddy77@gmail.com>
2020-11-22 10:39:11 -0500

    added edit and delete ability to posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -72,6 +72,66 @@
 
 		}
 
+		public function edit($id) {
+
+			if($_SERVER['REQUEST_METHOD'] == 'POST') {
+				// Sanitize post array
+				$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+				$data = [
+					'id' => $id,
+					'title' => trim($_POST['title']),
+					'body' => trim($_POST['body']),
+					'user_id' => $_SESSION['user_id'],
+					'title_err' => '',
+					'body_err' => '',
+				];
+
+				// Validate title
+				if (empty($data['title'])){
+					$data['title_err'] = 'Please enter a title';
+				}
+
+				// Validate body
+				if (empty($data['body'])){
+					$data['body_err'] = 'Please enter body text';
+				}
+
+				// Make sure no errors
+				if(empty($data['title_err']) && empty($data['body_err'])) {
+					// Validated
+					if($this->postModel->updatePost($data)) {
+						flash('post_message', 'Post Updated');
+						redirect('posts');
+					} else {
+						die('Something went wrong!');
+					}
+				}else {
+					// Load view with errors
+					$this->view('posts/edit', $data);
+				}
+
+
+			}else {
+				// Fetch the post 
+				$post = $this->postModel->getPostById($id);
+
+				// Check for owner 
+				if ($post->user_id != $_SESSION['user_id']){
+					redirect('posts');
+				}
+
+				$data = [
+					'id' => $id,
+					'title' => $post->title,
+					'body' => $post->body
+				];
+
+			$this->view('posts/edit', $data);
+			}
+
+		}
+
 		public function show($id) {
 			$post = $this->postModel->getPostById($id);
 			$user = $this->userModel->getUserById($post->user_id);
@@ -80,6 +140,26 @@
 				'user' => $user,
 			];
 			$this->view('posts/show', $data);
+		}
+
+		public function delete($id) {
+			if($_SERVER['REQUEST_METHOD'] == 'POST') {
+				// Fetch the post 
+				$post = $this->postModel->getPostById($id);
+
+				// Check for owner 
+				if ($post->user_id != $_SESSION['user_id']){
+					redirect('posts');
+				}
+				if($this->postModel->deletePOst($id)) {
+					flash('post_message', 'Post Removed');
+					redirect('posts');
+				}else {
+					die('Something Went Wrong!');
+				}
+			} else {
+				redirect('posts');
+			}
 		}
 
 	}
